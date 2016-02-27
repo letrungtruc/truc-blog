4fee5859dbbcc3e9d4572e24f3d0bbfa5549f679
TRUC LE <trucle@tinypulse.com>
2016-02-27 21:03:07 +0700

    add delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,6 +55,7 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
+    debugger
     @article.destroy
     respond_to do |format|
       format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
