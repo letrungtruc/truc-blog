0ed6628344924fbf7a0f27dccaae8393cbf6ae23
TRUC LE <trucle@tinypulse.com>
2016-02-27 21:11:29 +0700

    remove debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,6 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
-    debugger
     @article.destroy
     respond_to do |format|
       format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
